fe4696b23be464bf7e712ad4876dbc4d95074013
ramaniravi <ramaniravi196@gmail.com>
2024-08-28 10:27:50 +0530

    migrated to new project

---
+++ BulkyWeb-main/Bulky.DataAccess/Repository/ProductREpository.cs
@@ -0,0 +1,42 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.Models;
+using Bulky.DataAccess.Data;
+
+namespace Bulky.DataAccess.Repository
+{
+    internal class ProductREpository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductREpository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var productFromDb = _db.Products.FirstOrDefault(u => u.Id == product.Id);
+            if (productFromDb != null)
+            {
+                productFromDb.Title = product.Title;
+                productFromDb.ISBN = product.ISBN;
+                productFromDb.Price = product.Price;
+                productFromDb.Price50 = product.Price50;
+                productFromDb.ListPrice = product.ListPrice;
+                productFromDb.Price100 = product.Price100;
+                productFromDb.Description = product.Description;
+                productFromDb.CategoryId = product.CategoryId;
+                productFromDb.Author = product.Author;
+                if (product.ImageUrl != null)
+                {
+                    productFromDb.ImageUrl = product.ImageUrl;
+                }
+            }
+        }
+    }
+}
